63e3e8ab7cf22fa633a6dfe66f424cba5ce354fa
tabithatrubey <tabithatrubey@outlook.com>
2022-12-10 15:55:23 -0800

    CAN TIME SLOW DOWN

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -13,34 +13,93 @@
 // --------------------1) Create a function that takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
 
 // a) Create a test with an expect statement using the variable provided.
+describe("nameCapital", () => {
+  it ("takes in an array of objects and returns an array with a sentence about each person with their name capitalized", () => {
+      const people = [
+          { name: "ford prefect", occupation: "a hitchhiker" },
+          { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+          { name: "arthur dent", occupation: "a radio employee" }
+        ]
+     // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+      expect(nameCapital(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  
+  })
+})
+// ReferenceError: nameCapital is not defined
 
-const people = [
-  { name: "ford prefect", occupation: "a hitchhiker" },
-  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+const nameCapital = (array) => {
+  // Iterate over the array to access object properties.
+  // Use dot notation to access the object name values.
+  // Split the word with ' ', the space is intentional to split the words into two different strings so they can be evaluated or iterated over to capitalize the first letter of each first word in the strings and then add the remaining of each word not including the first letter to the Capital letter that was just generated and then join the strings back together to create one string again with the first and last name having capital letters. Then, add a space and the word is and the object occupation using dot notation and a period to render the expected outcome. 
+  return array.map(obj => obj.name.split(' ').map(word => {return word.charAt(0).toUpperCase() + word.slice(1)}).join(' ') + " " + "is" + " " + obj.occupation + ".") 
+      }
 
-// b) Create the function that makes the test pass.
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
+describe("onlyRemainders", () => {
+  it ("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.", () => {
+      const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+      // Expected output: [ 2, 0, -1, 0 ]
+      const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+      // Expected output: [ 2, 1, -1 ]
+     // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee"]
+      expect(onlyRemainders(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+      expect(onlyRemainders(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  
+  })
+})
+// ReferenceError: onlyRemainders is not defined
+// Filter through the array to determine if the value is a number
+    // If it is put it in an array
+    // Then divide each number and return their remainder
 
-const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
-// Expected output: [ 2, 0, -1, 0 ]
-const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
-// Expected output: [ 2, 1, -1 ]
+    // Create a function onlyRemainders that has one parameter: array
+const onlyRemainders = (array) => {
+  // Create a variable that will be set at the array we need to filter through the mix of numbers and strings and return only the type that are numbers.
+  let newArray = array.filter(element => typeof element === 'number') 
+  // Next, create a new variable that will set the numbers to be divided by 3 and map over the first variable applying the second variable's function to each number.
+      let divisibleNum = num => num % 3
+      return newArray.map(divisibleNum)
 
-// b) Create the function that makes the test pass.
+      
+  }
+  // Test Suites: 1 passed, 1 total
+  // Tests:       2 passed, 2 total
+
+
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
+describe("allCubed", () => {
+  it ("takes in an array of numbers and returns the sum of all the numbers cubed.", () => {
+    const cubeAndSum1 = [2, 3, 4]
+    // Expected output: 99
+    const cubeAndSum2 = [0, 5, 10]
+    // Expected output: 1125
+      expect(allCubed(cubeAndSum1)).toEqual(99)
+      expect(allCubed(cubeAndSum2)).toEqual(1125)
+  
+  })
+})
+// ReferenceError: allCubed is not defined
 
-const cubeAndSum1 = [2, 3, 4]
-// Expected output: 99
-const cubeAndSum2 = [0, 5, 10]
-// Expected output: 1125
 
-// b) Create the function that makes the test pass.
+// Create a function called allCubed that has one parameter: an array
+const allCubed = (array) => {
+  // Create a variable that will act as the container that will hold the end result, set at 0. 
+  let endingArray = 0
+  // Iterate through the array
+  for (let i=0; i<array.length; i++) {
+    // as the array is iterated over each index, cube the value, and add it to the above variable, endingArray.
+  endingArray += array[i] ** 3
+  }
+  // return the defined variable to get the end result of all index' cubed and added together.
+  return endingArray }
+
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -7,24 +7,25 @@
 # FILE: app/controller/blog_posts_controller.rb
 
 # ---1)
+# BlogPosts is the controller that was generated to communicate with the architecture of the rails application. This controller will facilitate interaction between the user, views, and model. The command that was ran to generate this controller was : $rails g controller BlogPosts
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2) Since we are in the index RESTful route, we are instantiated posts to give access from the method to the front-end. We are wanting ALL posts, which is why, posts is plural and not singular, as opposed to the show method below. This instantiation will always be plural. Following this instantiation, we can create a new html.erb file in the views folder that displays ruby code to display information. Index can act as the homepage.
     @posts = BlogPost.all
   end
 
-  # ---3)
+  # ---3) We are using the def show method to display only one instance, and the code below the declaration is searching for a specific blog with its associated id through the use of params which make rails dynamic.
   def show
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4) The 'new' RESTful route sends an HTML form for the user to fill out and the data is sent back to the database. This will allow a user to send in a form for a new blog post, if all the logic in the erb file is rendered properly. The associated route to this method would take the param as:'new_blog'. The new method does not require much on the controller, but on the views page, it should be ruby logic saturated. A programmer could embed Ruby logic in the index.html.erb file to place a link to add a new blog. 
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
+    # ---5) There is no view assoicated with this method, as create only connects from the controller to the model. This is referencing the blog post params which will take the same attributes that were entered when blog post was first created (i.e. title: string content:string). If the blog post is valid, meaning passes all requirements for blog post's attriubtes, then it will redirect to the path create @post in line 29. STRONG PARAMS. HERE'S WHAT YOU CAN SEND ME AND IF IT IS NOT VALID (TALK TO THE HAND) WE CANNOT COMMUNICATE!
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -32,13 +33,13 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    # ---6)
+    # ---6) This is going to send an HTML form similar to the 'new' route, but it updates an instance, at a given ID indicated by params:id, and sends it back to the database.
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7)This is going to update the blog on the create blog page (line 29), which means it will overwrite the existing instance and send it back to our database. 
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -48,15 +49,15 @@ class BlogPostsController < ApplicationController
   def destroy
     @post = BlogPost.find(params[:id])
     if @post.destroy
-      # ---8)
+      # ---8) If the certain blog post that was passed into the params id has been destroyed, then you will be redirected to the create blog post page as indicated in line 29.
       redirect_to blog_posts_path
     end
   end
 
-  # ---9)
+  # ---9)The purpose of this method in the private route is for internal authorization only and cannot be modified. It cannot be modified by an unauthorized user who gained access to the database.
   private
   def blog_post_params
-    # ---10)
+    # ---10) The strong params say here's what you're allowed to send from the .permit and referencing the blog_post model. This cannot be altered by an unauthorized user.
     params.require(:blog_post).permit(:title, :content)
   end
 end
